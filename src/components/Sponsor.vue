bbb4eff3d42024d7c99b16645eb610f8be433827
NoSineB <132141781+NoSineB@users.noreply.github.com>
2024-03-30 13:47:18 +0530

    First Commit

---
+++ src/components/Sponsor.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="mt-20 mb-10">
+        <div class="font-sans text-[#00FFA3] text-6xl" v-motion-slide-visible-bottom>
+            Sponsors
+        </div>
+    </div>
+    <div class="mt-2"><a href=""
+            class="p-2 text-[#00FFA3] rounded-2xl border-2 border-[#00FFA3] hover:text-white hover:bg-[#1DCC7E] duration-500 px-5">Become
+            a
+            Sponsor</a>
+    </div>
+    <div class="mt-10 flex flex-col lg:flex-row">
+        <div class="bg-white w-[350px] lg:w-[400px] me-0 lg:me-6 mb-4 p-4 rounded-md" v-motion-slide-visible-bottom>
+            <img src="../assets/images/QL black logo.png" alt="">
+        </div>
+        <div class="bg-white w-[350px] lg:w-[400px] me-0 lg:me-6 mb-4 p-4 rounded-md" v-motion-slide-visible-bottom>
+            <img src="../assets/images/Devfolio - Normal.svg" alt="">
+        </div>
+    </div>
+    <div class="flex flex-col lg:flex-row">
+        <div class="bg-white me-0 lg:me-6 mb-4 p-4 rounded-md flex justify-evenly items-center "
+            v-motion-slide-visible-bottom>
+            <img src="../assets/images/SB Logo.png" class="w-[350px] lg:w-[450px]" alt="">
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import './assets/tailwind.css'
+import { MotionPlugin } from '@vueuse/motion'
+
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+const app =  createApp(App)
+
+app.use(MotionPlugin)
+app.mount('#app')
