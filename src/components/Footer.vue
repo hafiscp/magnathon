4bd2bce959ae74bd57952f6801dea731cac13547
NoSineB <132141781+NoSineB@users.noreply.github.com>
2024-04-08 20:40:46 +0530

    Footer Updated to IEEE SB CEV

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="my-8 bg-[#031d44] text-[#fffff2] w-full flex justify-center items-center p-2">
-        Made with ❤️ By	&nbsp;<a href="https://www.linkedin.com/in/benison-abraham-1a504b252/" class="hover:text-[#F87666]">Benison</a>
+    <div class="mt-8 py-4 bg-[#031d44] text-[#fffff2] w-full flex justify-center items-center p-2">
+        Made with ❤️ By	&nbsp;<a href="https://www.ieeesbcev.org/" class="hover:text-[#F87666]">IEEE SB CEV WEB TEAM</a>
     </div>
 </template>
